3ec503ecb6da75a974a162200ba40abb864e1fc6
Zhydani <zhydani@gmail.com>
2023-03-24 21:59:06 -0300

    tryind to add navigation component but is not easy

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,23 +2,32 @@ import {
     Divider
 } from "@react-native-material/core";
 import { React } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import IconSet from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+// import HomeScreen from "../screens/HomeScreen";
   
-function Menu() {
+function Menu({ navigation }) {
     return (
         <View style={styles.menu}>
         <View style={styles.drawerContent}>
             <Text style={styles.drawerText}>Menu</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <View style={styles.menuItem}>
+                    <Icon name="home" size={24} color="white" />
+                    <Text style={styles.menuItemText}>Home</Text>
+                </View>
+            </TouchableOpacity>
+            <Divider style={{ marginBottom: 10 }} leadingInset={45} />
             <View style={styles.menuItem}>
-            <Icon name="contacts" size={24} color="white" />
-            <Text style={styles.menuItemText}>Contatos</Text>
+                <Icon name="contacts" size={24} color="white" />
+                <Text style={styles.menuItemText}>Contatos</Text>
             </View>
             <Divider style={{ marginBottom: 10 }} leadingInset={45} />
             <View style={styles.menuItem}>
-            <IconSet name="settings-sharp" size={24} color="white" />
-            <Text style={styles.menuItemText}>Configurações</Text>
+                <IconSet name="settings-sharp" size={24} color="white" />
+                <Text style={styles.menuItemText}>Configurações</Text>
             </View>
         </View>
         </View>
