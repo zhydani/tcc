b545844161688eab17d56d528749f222a983be47
Zhydani <zhydani@gmail.com>
2023-03-28 08:47:26 -0300

    add modal contacts

---
+++ src/screens/contact/ModalContacts.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { FlatList, Modal, Pressable, Text, TextInput, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { getAllContacts, keyExtractor } from '../../controllers/ContactController';
+import requestContactsPermission from "../../utils/ContactPermission";
+import ContactItem from './ContactItem';
+import styles from './Styles';
+
+
+function ModalContacts() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    requestContactsPermission();
+    getAllContacts().then(contacts => {
+      if (contacts.length > 0) {
+        setContacts(contacts);
+      }
+    })
+  }, []);
+
+  function openContactsModal() {
+    setModalVisible(true);
+  };
+  
+  const renderItem = ({item, index}) => {
+    return <ContactItem contact={item} />;
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={openContactsModal}>
+        <Text style={styles.textStyle}><Icon name="plus" size={18} color="white"/> Adicionar Contato</Text>
+      </Pressable>
+      <Modal style={styles.centeredView} animationType="slide" visible={modalVisible} transparent>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={styles.inputView}>
+              <Ionicons name="search" size={20} color="#ccc" />
+              <TextInput
+                style={styles.input}
+                // onChangeText={onChangeNumber}
+                // value={number}
+                placeholder="Buscar contato"
+                keyboardType="default"
+              />
+            </View>
+            <FlatList
+              data={contacts}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
+              style={styles.list}
+            />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(false)}>
+              <Text style={styles.textStyle}>Fechar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default ModalContacts;
--- src/screens/contact/index.js
+++ src/screens/contact/index.js
@@ -1,71 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, Modal, Pressable, Text, TextInput, View } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState } from 'react';
 import HeaderComponent from "../../components/HeaderComponent";
-import { getAllContacts, keyExtractor } from '../../controllers/ContactController';
-import requestContactsPermission from "../../utils/ContactPermission";
-import ContactItem from './ContactItem';
-import styles from './Styles';
-
+import ModalContacts from './ModalContacts';
+// import styles from './Styles';
 
 function ContactManageContent() {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [contacts, setContacts] = useState([]);
   const [contactsList, setContactsList] = useState([]);
 
-  useEffect(() => {
-    requestContactsPermission();
-    getAllContacts().then(contacts => {
-      if (contacts.length > 0) {
-        setContacts(contacts);
-      }
-    })
-  }, []);
-
-  function openContactsModal() {
-    setModalVisible(true);
-  };
-  
-  const renderItem = ({item, index}) => {
-    return <ContactItem contact={item} />;
-  };
-
   return (
-    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={openContactsModal}>
-        <Text style={styles.textStyle}><Icon name="plus" size={18} color="white"/> Adicionar Contato</Text>
-      </Pressable>
-      <Modal style={styles.centeredView} animationType="slide" visible={modalVisible} transparent>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <View style={styles.inputView}>
-              <Ionicons name="search" size={20} color="#ccc" />
-              <TextInput
-                style={styles.input}
-                // onChangeText={onChangeNumber}
-                // value={number}
-                placeholder="Buscar contato"
-                keyboardType="default"
-              />
-            </View>
-            <FlatList
-              data={contacts}
-              renderItem={renderItem}
-              keyExtractor={keyExtractor}
-              style={styles.list}
-            />
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(false)}>
-              <Text style={styles.textStyle}>Fechar</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-    </View>
+    <ModalContacts />
   );
 };
 
@@ -74,6 +16,5 @@ function ContactManageScreen({navigation}) {
     <HeaderComponent subheader="Contatos" content={<ContactManageContent />} navigation={navigation}/>
   );
 };
-
 
 export default ContactManageScreen;
