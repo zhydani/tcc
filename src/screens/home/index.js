f63632366a59288a23723067d235b2339ef5462f
Zhydani <zhydani@gmail.com>
2023-04-19 14:52:13 -0300

    send message format with localization to whatsapp contacts

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect } from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
@@ -7,7 +8,13 @@ import requestPermission from "../../utils/LocalizationPermission";
 import styles from './Styles';
 
 const HomeContent = () => {
+  
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  
   const handleButtonPress = async () => {
+    const contacts = await getSavedContacts();
     // Obter as coordenadas de latitude e longitude do dispositivo
     Geolocation.getCurrentPosition(
       position => {
@@ -15,20 +22,32 @@ const HomeContent = () => {
         const lon = position.coords.longitude;
 
         // Criar uma mensagem formatada com um link do Google Maps
-        const message = `Socorro! Estou em perigo! Por favor, me ajude!\n\nMinha localização atual: https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
+        const message = `Socorro! Estou em perigo. Minha localização é:\n\nMinha localização atual: https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
 
         // Abrir o aplicativo do WhatsApp e compartilhar a mensagem formatada
-        // Linking.openURL(`whatsapp://send?text=${encodeURIComponent(message)}`);
-        console.log(message)
+        const contactsWithPhone = contacts.filter((c) => c?.phoneNumbers && c?.phoneNumbers.length > 0);
+        const numbers = contactsWithPhone.map((c) => c?.phoneNumbers[0].number);
+        
+        // Linking.openURL(`whatsapp://send?text=${encodeURIComponent(message)}&phone=${numbers.join(',')}`);
+        console.log(message + " numeros enviados:  " + numbers.join(','))
       },
       error => alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   };
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
+  async function getSavedContacts() {
+    try {
+      const contacts = await AsyncStorage.getItem('@contacts');
+      if (contacts !== null) {
+        return JSON.parse(contacts);
+      }
+      return [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
 
   return (
     <View style={styles.container}>
